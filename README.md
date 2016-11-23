38b0b53eac27e15a7a98ddc0013213ae2826d9ce
Espen Fossen <espenaf@junta.no>
2016-11-23 19:20:39 +0100

    Adapt to SoS 2017.

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 * Pick/generate a password and BCrypt it.
 * Put the hashed password in ems' password.properties. `submitit.redux=#hash`.
 * Put the plain text version in cake config and submitit config in property emsPassword.
-* Make sure the Cake app is not exposed with out protection. Either put something in front of it or configure Google OAuth.
+* Make sure the Cake app is not exposed without protection. Either put something in front of it or configure Google OAuth.
 * Run buildall.sh then docker-compose -f compose/docker-compose.yml up
--- buildall.sh
+++ buildall.sh
@@ -2,43 +2,43 @@
 # requires sbt, lein, mvn, docker, docker-compose
 mkdir -p code
 cd code
-git clone git@github.com:computerlove/ems-redux.git
+git clone git@github.com:espenaf/ems-redux.git
 cd ems-redux
 git checkout TDC
 sbt appmgr:packageBin
 # target/appmgr
 cd ..
-git clone git@github.com:computerlove/submitit-redux.git
+git clone git@github.com:espenaf/submitit-redux.git
 cd submitit-redux
-git checkout TDC
+git checkout SOS
 lein uberjar
-# target/submitit-1.0.TDC-standalone.jar
+# target/submitit-1.0.SOS-standalone.jar
 cd ..
-git clone git@github.com:computerlove/cake-redux.git
+git clone git@github.com:espenaf/cake-redux.git
 cd cake-redux
-git checkout TDC
+git checkout SOS
 mvn install -Dmaven.test.skip
-# target/cake-redux-1.0.TDC-jar-with-dependencies.jar
+# target/cake-redux-1.0.SOS-jar-with-dependencies.jar
 cd ..
-git clone git@github.com:computerlove/javazone-web-api.git
+git clone git@github.com:espenaf/javazone-web-api.git
 cd javazone-web-api
 git checkout TDC
 mvn install -Dmaven.test.skip
-# target/javazone-web-api-1.0-TDC-appmgr.zip
+# target/javazone-web-api-1.0-SOS-appmgr.zip
 cd ../..
 # Dockerize
-docker build -t ems-postgresql postgres
+sudo docker build -t ems-postgresql postgres
 cp -R code/ems-redux/target/appmgr ems/ems
-docker build -t ems ems
+sudo docker build -t ems ems
 rm -rf ems/ems
-cp code/cake-redux/target/cake-redux-1.0.TDC-jar-with-dependencies.jar cake/cake-redux.jar
-docker build -t cake cake
+cp code/cake-redux/target/cake-redux-1.0.SOS-jar-with-dependencies.jar cake/cake-redux.jar
+sudo docker build -t cake cake
 rm cake/cake-redux.jar
 mkdir -p api/api
 unzip code/javazone-web-api/target/javazone-web-api-1.0-TDC-appmgr.zip -d api/api
-docker build -t api api
+sudo docker build -t api api
 rm -rf api/api
-cp code/submitit-redux/target/submitit-1.0.TDC-standalone.jar submittit/submitit.jar
-docker build -t submitit submittit
+cp code/submitit-redux/target/submitit-1.0.SOS-standalone.jar submittit/submitit.jar
+sudo docker build -t submitit submittit
 rm submittit/submitit.jar
 echo "Now run docker-compose -f compose/docker-compose.yml up"
